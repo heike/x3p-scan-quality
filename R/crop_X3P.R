ab0381d7a62f001e68944a468ad102f6ed30dee4
ArgentCode <craigorman0@gmail.com>
2023-02-01 11:37:20 -0600

    Fixed the crop function, now crops from the right as well

--- R/crop_X3P.R
+++ R/crop_X3P.R
@@ -1,6 +1,8 @@
 crop_X3P <- function(x3p) {
   stopifnot(class(x3p) == "x3p")
   dims <- dim(x3p$surface.matrix)
-  cropped_x3p <- x3p_crop(x3p, x = 0.1 * dims[1], y = 0.1 * dims[2], width = dims[1] - (0.1 * dims[1]), height = dims[2] - (0.1* dims[2]))
+  cropped_x3p <- x3p_crop(x3p, x = 0.1 * dims[1], y = 0.1 * dims[2], width = dims[1], height = dims[2] - (0.1* dims[2]))
+  dims <- dim(cropped_x3p$surface.matrix)
+  cropped_x3p <- x3p_crop(cropped_x3p, x = 0, width = dims[1] - (0.1 * dims[1]), y = 0, height = dims[2])
   return(cropped_x3p)
 }
